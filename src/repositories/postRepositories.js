c511fced4595b696b976ca2f98b4e7c6f9fb8811
saulo <saulodavi2001@gmail.com>
2022-10-24 15:22:03 -0300

    merge by saulo

--- src/repositories/postRepositories.js
+++ src/repositories/postRepositories.js
@@ -1,55 +1,24 @@
 import { connection } from "../database/database.js";
 
-async function insertPost(userId, text, link) {
+async function insertPost(email) {
     const query = `INSERT INTO posts ("userId", text, link) VALUES ($1, $2, $3)`
-
-    return connection.query(query, [userId, text, link])
-   
-}
-async function insertLike(userId, postId, liked) {
-    const query = `INSERT INTO likes ( "userId", "postId", liked) VALUES ($1, $2, $3)`
-    return connection.query(query, [userId, postId,liked])
-   
+    return connection.query(query, [email])
 }
 
-async function getHashtagIdByName(atual) {
-    console.log("entrei")
-    const query = "SELECT (id) from hashtags WHERE name = $1"
-    return connection.query(query, [atual])
+async function getHashtagByName(email) {
+    const query = `INSERT INTO posts ("userId", text, link) VALUES ($1, $2, $3)`
+    return connection.query(query, [email])
 }
-
-async function getPostId(userId, text, link) {
-    
-    const query =  'SELECT (id) from posts WHERE "userId" = $1 AND text = $2 AND link = $3 ORDER BY "createdAt" DESC LIMIT 1'
-    return connection.query(query, [userId, text, link])
-}
-
-async function insertHashtag(atual) {
-    const query = "INSERT INTO hashtags (name) VALUES ($1)"
-    return connection.query(query, [atual])
-}
-
-// 
-
 
 async function deletePost(id){
     const query = `DELETE FROM posts WHERE id = $1`
     return connection.query(query, [id])
-
 }
 
 const postRepository = {
     insertPost,
-    getHashtagIdByName,
-    getPostId,
-    insertHashtag,
-    insertLike,
-
-
-
+    getHashtagByName,
     deletePost
-    
-
 }
 
 export { postRepository }
